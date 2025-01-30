5c6a3b66c7e5e28f1307cf2c6e023441ee597e61
Kodex-Baba <122707428+Kodex-Baba@users.noreply.github.com>
2025-01-30 13:48:41 -0500

    Week04 Mongoose done

---
+++ week4_mongodb/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const employeeRouter = require('./routes/EmployeeRoutes.js');
+
+const app = express();
+app.use(express.json()); // Make sure it comes back as json
+
+//TODO - Replace you Connection String here
+mongoose.connect('mongodb+srv://kodexbaba41:Ea4312016085*@cluster0.o7wsr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(success => {
+  console.log('Success Mongodb connection')
+}).catch(err => {
+  console.log('Error Mongodb connection')
+});
+
+app.use(employeeRouter);
+
+app.listen(8081, () => { console.log('Server is running...') });
